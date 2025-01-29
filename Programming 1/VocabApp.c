86be3926b8192bd364b99e2275f5e2968862ca83
MatthepPro5967 <148478297+MatthepPro5967@users.noreply.github.com>
2025-01-29 12:28:13 -0500

    Create VocabApp.c

---
+++ Programming 1/VocabApp.c
@@ -0,0 +1,275 @@
+/******************************************************************************
+
+Vocabulary APP
+2/26/2024
+Matthew Sliman
+
+*******************************************************************************/
+#include <stdio.h>
+#include "stdlib.h"
+#include "time.h"
+
+void VictorySign(int random)
+{   //input: int radom
+    //returned value: none
+    //purpose: based on value, output a design
+   
+    if(random == 1)
+    {
+        printf("__________________________________________________________________________\n");        
+        printf("                                          _.-""""""""""'-----...      ______ `.\n");            
+        printf("                                       .-'                      l ,-''    \\ '-.`.\n");          
+        printf("                                    .-'                         ; ;        ;   \\ ""--.._\n");   
+        printf("                                  .'                           : :         |    ;      .|\n");  
+        printf("                            _.._.'                             ; ;  ___    |    ;    .' :\n");  
+        printf("                           (  .'                              : :  :   '.  :..-'   .'    ;\n"); 
+        printf("                            )'                                | ;  ; __.'-'(     .'  .--.:\n"); 
+        printf("                    ___...-''''----....____          ______.-' :-/.'       \\_.-'  .' .-.\\|\n");
+        printf("            __..--''                        ''''''''''          /\\'          ;    / .gs./\\;\n");
+        printf("        _.-'                                                   /  ;          |   . d$P'Tb\n");  
+        printf("     .-""-,                       ____                        /   |          :   ;:$$   $;\n"); 
+        printf("   .'     ;                    ,''    ""--..__               /    :          |   $$$;   :$\n"); 
+        printf("  /-'._  /                     ;       ____..-'    .-""'-.  /     :          ;  _$$$;   :$\n"); 
+        printf(" :     ''--.._          ___....+---""""          .'  _._  \\/      |         _:-' $$$;   :$\n"); 
+        printf(" ;                                              /  .d$$$b./       ;      .-'.'   :$$$   $P\n"); 
+        printf(":            .----...____                      :  dP' `T$P        |   .-' .' _.gd$$$$b_d$'\n"); 
+        printf(";    __...---| GOOD JOB! |----....____         | :$     $b        : .'   (.-'  `T$$$$$$P'\n");  
+        printf(";  .';       '----...____;       /    '-.      ; $;     :$;_____..-'  .-'\n");                  
+        printf(": /  :                          /        \\__..-':$       $$ ;-.    .-'\n");                     
+        printf(" Y    ;                        /          ;     $;       :$;|  `.-'\n");                        
+        printf(" :    :                       /           |     $$       $$;:.-'\n");                           
+        printf(" '$$$ggggp...____            /            :     :$;     :$$\n");                                
+        printf("  $$$$$$$$$$$$   """"----...:________....gggg$$$$$$     $$;\n");                                
+        printf("  'T$$$$$$$$P'                           T$$$$$$$$$b._.d$P\n");                                 
+        printf("    `T$$$$P'                              T$$$$$$$$$$$$$P\n");                                  
+        printf("                                           `T$$$$$$$$$P'\n");    
+        printf("__________________________________________________________________________\n");    
+    }
+    if(random == 2)
+    {
+        printf("__________________________________________________________________________\n");
+        printf(".-------------------------------------------------------------.\n");
+        printf("'------..-------------..----------..----------..----------..--.|\n");
+        printf("|       \\\\            ||          ||          ||          ||  ||\n");
+        printf("|        \\\\           ||          ||          ||          ||  ||\n");
+        printf("|    ..   ||  _    _  ||    _   _ || _    _   ||    _    _||  ||\n");
+        printf("|    ||   || //   //  ||   //  // ||//   //   ||   //   //|| /||\n");
+        printf("|_.------''----------''----------''----------''----------''--'|\n");
+        printf(" |)|      |       |       |       |    |         |      ||==|  |\n");
+        printf(" | |      |  _-_  |       |       |    |  .-.    |      ||==| C|\n");
+        printf(" | |  __  |.'.-.' |   _   |   _   |    |.'.-.'.  |  __  | '__=='\n");
+        printf(" '---------'|( )|'----------------------'|( )|'----------''\n");
+        printf("             '-'                          '-'\n");
+        printf(" _____ _________________ _____ _____ _____ _ \n");
+        printf("/  __ \\  _  | ___ \\ ___ \\  ___/  __ \\_   _| |\n");
+        printf("| /  \\/ | | | |_/ / |_/ / |__ | /  \\/ | | | |\n");
+        printf("| |   | | | |    /|    /|  __|| |     | | | |\n");
+        printf("| \\__/\\ \\_/ / |\\ \\| |\\ \\| |___| \\__/\\ | | |_|\n");
+        printf(" \\____/\\___/\\_| \\_\\_| \\_\\____/ \\____/ \\_/ (_)\n");
+        printf("__________________________________________________________________________\n");                                             
+    }
+    if(random == 3)
+    {
+        printf("__________________________________________________________________________\n");
+        printf("                      ___\n"); 
+        printf("                      \\\\ \\\n"); 
+        printf("                       \\\\ `\\\n"); 
+        printf("    ___                 \\\\  \\\n"); 
+        printf("   |    \\                \\\\  `\\\n"); 
+        printf("   |_____\\                \\    \\\n"); 
+        printf("   |______\\                \\    `\\\n"); 
+        printf("   |       \\                \\     \\\n"); 
+        printf("   |      __\\__---------------------------------._.\n"); 
+        printf(" __|---~~~__o_o_o_o_o_o_o_o_o_o_o_o_o_o_o_o_o_o_[][\\__\n"); 
+        printf("|___          AMAZING JOB!  /~      ) YOU ARE SMART!! \\__\n"); 
+        printf("    ~~~---..._______________/      ,/_________________/\n"); 
+        printf("                           /      /\n"); 
+        printf("                          /     ,/\n"); 
+        printf("                         /     /\n"); 
+        printf("                        /    ,/\n"); 
+        printf("                       /    /\n"); 
+        printf("                      //  ,/\n"); 
+        printf("                     //  /\n"); 
+        printf("                    // ,/\n"); 
+        printf("                   //_/\n"); 
+        printf("__________________________________________________________________________\n");
+    }
+    if(random == 4)
+    {
+        printf("__________________________________________________________________________\n");
+        printf("                           v  ~.      v\n"); 
+        printf("          v           /|\n"); 
+        printf("                     / |          v\n"); 
+        printf("              v     /__|__\n"); 
+        printf("                  \\\\--------/\n"); 
+        printf("~~~~~~~~~~~~~~~~~~~`~~~~~~'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"); 
+        printf("Wish you were here?... (Good job btw)\n");     
+        printf("__________________________________________________________________________\n");    
+    }
+    if(random == 5)
+    {
+        printf("__________________________________________________________________________\n");    
+        printf("   ___     _       ___   __   __   ___    _____    ___     ___     ___    _  _     ___\n");   
+        printf("  |_ _|   | |     / _ \\  \\ \\ / /  | __|  |_   _|  | _ \\   /   \\   |_ _|  | \\| |   / __|\n");  
+        printf("   | |    | |__  | (_) |  \\ V /   | _|     | |    |   /   | - |    | |   | .` |   \\__ \\\n");  
+        printf("  |___|   |____|  \\___/   _\\_/_   |___|   _|_|_   |_|_\\   |_|_|   |___|  |_|\\_|   |___/\n");  
+        printf("_|'''''|_|'''''|_|'''''|_| ''''|_|'''''|_|'''''|_|'''''|_|'''''|_|'''''|_|'''''|_|'''''|\n"); 
+        printf("'`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-''`-0-0-'\n");
+        printf("__________________________________________________________________________\n");    
+
+    }
+}
+void GetUserGuess(char s[])
+{   //input: char s
+    //returned value: none
+    //purpose: get user guess and correct it
+    
+    char guess[20];
+   
+    int counter = 0;
+    char LOWER[] = "abcdefghijklmnopqrstuvwxyz";
+    char UPPER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int ggg = 0; //separate counter to keep track of upper and lowecase letters
+   
+   
+   do
+   {
+        ggg = 0;
+        
+        for(int n = 0; s[n] != '\0'; ++n) //set counter = to number of letters of vocab word
+            counter = n + 1;
+        //printf("counter00000 = %i", counter);
+        
+        printf("Enter guess: ");
+        scanf("%s", guess);
+    
+        
+        do
+        {
+            for(int n = 0; UPPER[n] != '\0'; ++n)
+            {
+                if(UPPER[n] == guess[ggg])
+                {
+                    guess[ggg] = LOWER[n];
+                    //printf("\n%c = %c", guess[ggg], LOWER[n]);
+                    //printf("guess[ggg] = %c", guess[ggg]);
+                }
+            
+    
+            }
+            ++ggg;
+            //printf("\nggg = %i", ggg);
+        }while(ggg <= counter);
+        
+        ggg = 0;
+        
+        for(int n = 0; guess[ggg] != '\0'; ++n)
+        {
+            if(guess[ggg] == s[n])
+            {
+                counter -= 1;
+                ++ggg;
+            }
+            else
+            {
+                counter += 1;
+                ++ggg;
+            }
+        }
+        //printf("\ncounter222 = %i", counter);
+   }while(counter != 0);
+    
+    
+    
+    
+}
+void DisplayDefinitions(int num)
+{   //input: int num
+    //returned value: none
+    //purpose: display a certain definition based on num 
+    
+    switch(num)
+    {
+        case 1: printf("A four-wheeled road vehicle that is powered by an engine and is able to carry ");
+                printf("a small number of people.\n"); break;
+        case 2: printf("A large motor vehicle carrying passengers by road, typically one serving the public ");
+                printf("on a fixed route and for a fare.\n"); break;
+        case 3: printf("A powered flying vehicle with fixed wings and a weight greater than that of the air ");
+                printf("it displaces.\n"); break;
+        case 4: printf("A small vessel propelled on water by oars, sails, or an engine.\n"); break;
+        case 5: printf("A series of railroad cars moved as a unit by a locomotive or by integral motors.\n"); break;
+    }
+    
+}
+void PrintArray(int A[])
+{   //input: array questions
+    //returned value: none
+    //purpose: print array values
+    
+    for(int n = 1; n <= 5; ++n)
+    {
+        printf("\t%i", A[n]);
+    }
+    printf("\n");
+}
+void SetAnswers(int num)
+{   //input: int num
+    //returned value: none
+    //purpose: set answers
+    
+    char answer1[] = "car";
+    char answer2[] = "bus";
+    char answer3[] = "airplane";
+    char answer4[] = "boat";
+    char answer5[] = "train";
+    
+    switch(num)
+    {
+        case 1: GetUserGuess(answer1); VictorySign(num); break;
+        case 2: GetUserGuess(answer2); VictorySign(num); break;
+        case 3: GetUserGuess(answer3); VictorySign(num); break;
+        case 4: GetUserGuess(answer4); VictorySign(num); break;
+        case 5: GetUserGuess(answer5); VictorySign(num); break;
+    }
+    
+    
+    
+}
+
+int main()
+{
+    srand(time(NULL)); //random number
+    
+    int value; //used to determine chosen definition
+    int Questions[6] = {0, 1, 2, 3, 4, 5}; //array to keep track of each definition
+    int count = 0;
+ 
+    //value = (int)(rand()%5) + 1; //random number generator to generate int value 1 - 5
+
+    do
+    {
+        value = (int)(rand()%5) + 1;
+        
+        if(Questions[value] != 0)
+        {
+            DisplayDefinitions(value);
+            SetAnswers(value);
+            count += 1;
+        }
+        Questions[value] = 0;
+        //PrintArray(Questions);
+
+
+    }while(count < 5);
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
