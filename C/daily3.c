517022754af84bfa6137b542c01f194ac8e79ed3
MatthepPro5967 <148478297+MatthepPro5967@users.noreply.github.com>
2025-09-17 19:44:17 -0400

    Add files via upload

---
+++ C/daily3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    char name[] = "Matthew";
+
+    printf("Hello my name is %s. This class is \"Awesome Sauce\"!\n", name);
+    printf("Something about me: I can do around 25 pull-ups in a row.");
+
+}
